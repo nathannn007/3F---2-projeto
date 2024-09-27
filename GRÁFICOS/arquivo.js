c78e6cac08505550d336a79d20a6fd97e20134d7
nathannn007 <106194240+nathannn007@users.noreply.github.com>
2024-09-27 08:55:52 -0300

    Add files via upload

--- GRÁFICOS/arquivo.js
+++ GRÁFICOS/arquivo.js
@@ -1,3 +1,15 @@
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
-console.log(url);   
+async function visualizaDadosGlobais(){
+    const res = await fetch(url);
+    console.log(res);
+    const dados = await res.json()
+    console.log(dados);
+    const paragrafo = document.createElement('p')
+    //*cria uma tag 'p' que é um paragrafo(no HTML)*//
+    paragrafo.classList.add('graficos-texto')
+    //*cria uma classe 'graficos-texto' na tag 'p'
+    const container = document.getElementById('graficos-container')
+}
+
+visualizaDadosGlobais();
