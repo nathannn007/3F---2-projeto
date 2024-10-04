8c737c92c109d3049e05853cd2ec65f498894c66
nathannn007 <106194240+nathannn007@users.noreply.github.com>
2024-10-04 08:56:49 -0300

    Add files via upload

--- GRÁFICOS/arquivo.js
+++ GRÁFICOS/arquivo.js
@@ -4,8 +4,8 @@ async function visualizaDadosGlobais(){
     const res = await fetch(url);
     console.log(res);
     const dados = await res.json()
-    const totalPessoasMundo = (dados.total_Pessoas_Mundo)/1e9;
-    const totalPessoasConectadas = (dados.total_Pessoas_Conectadas)/1e9;
+    const totalPessoasMundo = (dados.total_pessoas_mundo)/1e9;
+    const totalPessoasConectadas = (dados.total_pessoas_conectadas)/1e9;
     const tempoMedio = dados.tempo_medio;
     console.log(dados);
 
@@ -14,7 +14,7 @@ async function visualizaDadosGlobais(){
     paragrafo.classList.add('graficos-texto')
     //*cria uma classe 'graficos-texto' na tag 'p'
     const container = document.getElementById('graficos-container')
-    paragrafo.innerHTML = `Meu mundo tem ${totalPessoasMundo} bilhões de habitantes, dos quais ${totalPessoasConectadas} bilhões estão conectadas em alguma rede social por um tempo médio de ${tempoMedio}.`
+    paragrafo.innerHTML = `Meu mundo tem <span> ${totalPessoasMundo} bilhões </span> de habitantes, dos quais <span> ${totalPessoasConectadas} bilhões </span> estão conectadas em alguma rede social por um tempo médio de <span> ${tempoMedio}</span>.`
     container.appendChild(paragrafo);
 }
 
